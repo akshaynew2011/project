9ff9686b2971418460a15bcc6c43dc159856d400
Akshay Sekar <akshaynew2011@hotmail.com>
2019-05-12 13:14:46 -0500

    latest changes

---
+++ simple-java-maven-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,34 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ */
+public class App
+{
+
+    private final String message = "Hello World to Akshay Goyal the Don!";
+    public static Integer add(Integer a, Integer b) {
+        if (a == null) {
+            return null;
+        }
+        if (b == null) {
+            return null;
+        }
+        return a + b;
+    }
+
+    public App() {}
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b=20;
+        System.out.println(new App().getMessage());
+        System.out.println(new App().add(a,b));
+    }
+
+    private final String getMessage() {
+        return message;
+    }
+
+
+}
---
+++ simple-java-maven-app/src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,61 @@
+package com.mycompany.app;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.After;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Test
+    public void testAppConstructor() {
+        try {
+            new App();
+        } catch (Exception e) {
+            fail("Construction failed.");
+        }
+    }
+
+    @Test
+    public void testAppMain()
+    {
+        App.main(null);
+        try {
+            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
+        } catch (AssertionError e) {
+            fail("\"message\" is not \"Hello World!\"");
+        }
+    }
+
+    @Test
+    public void testAdd() {
+        System.out.println("add");
+        Integer a = 15;
+        Integer b = 15;
+        Integer expResult = 30;
+        Integer result = Util.add(a, b);
+        assertEquals(expResult, result);
+        assertEquals(null, Util.add(15, null));
+        assertEquals(null, Util.add(null, 15));
+        assertEquals(null, Util.add(null, null));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+
+}
