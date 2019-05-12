b441b1abfd5e12362e7181aed7bc6c7469663563
Akshay Sekar <akshaynew2011@hotmail.com>
2019-05-12 16:06:55 -0500

    change

--- simple-java-maven-app/src/main/java/com/mycompany/app/App.java
+++ simple-java-maven-app/src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World g hghgc";
+    private final String message = "Hello World Akshay";
     public static Integer add(Integer a, Integer b) {
         if (a == null) {
             return null;
